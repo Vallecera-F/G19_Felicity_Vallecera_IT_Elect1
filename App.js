08860c6e0b12a6cd1c9049c7a387f246c9a21834
Vallecera_F/github_username <felicityvallecera@gmail.com>
2025-09-20 21:19:19 +0800

    Added CounterApp, ColorChangerApp, and MidtermAct01

---
+++ App.js
@@ -0,0 +1,16 @@
+import ColorChangerApp from './ColorChangerApp';
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import CounterApp from './CounterApp';  
+import MidtermAct01 from './MidtermAct01';
+
+export default function App() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+     <MidtermAct01/>
+     <ColorChangerApp/>
+     <CounterApp/>
+ 
+    </SafeAreaView>
+  );
+}
