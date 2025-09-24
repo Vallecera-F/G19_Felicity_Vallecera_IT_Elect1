06fc47f83992e2171b348b029bd1523f44e21a98
Vallecera_F/github_username <felicityvallecera@gmail.com>
2025-09-25 01:14:20 +0800

    Update App.js and Activity5 folder

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
-import ColorChangerApp from './ColorChangerApp';
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import CounterApp from './CounterApp';  
-import MidtermAct01 from './MidtermAct01/MidtermAct01';
-
+import MessengerApp from './Activity5/ColorChangerApp';
+import ColorChangerApp from './Activity5/CounterApp';
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-     <MidtermAct01/>
-     <ColorChangerApp/>
-     <CounterApp/>
- 
+       <ColorChangerApp/>
+    <CounterApp/>
     </SafeAreaView>
   );
 }
