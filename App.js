7fb2536c8ec1f83084bce92b30d24fb786c78bf8
Vallecera_F/github_username <felicityvallecera@gmail.com>
2025-09-25 00:39:19 +0800

    Refactor: replace old components with new MessengerApp and CommentApp

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
-import ColorChangerApp from './ColorChangerApp';
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import CounterApp from './CounterApp';  
-import MidtermAct01 from './MidtermAct01';
-
+import MessengerApp from './MidAct1/MessengerApp';
+import CommentApp from './MidAct1/CommentApp';
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-     <MidtermAct01/>
-     <ColorChangerApp/>
-     <CounterApp/>
- 
+       <CommentApp/>
+    <MessengerApp/>
     </SafeAreaView>
   );
 }
